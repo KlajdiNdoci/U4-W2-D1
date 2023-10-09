7625597b2eb77b7ec8340fe1d3406d28d3be66a9
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-10-09 14:44:21 +0200

    Exercise 1 completed

---
+++ src/Exercise1.java
@@ -0,0 +1,43 @@
+package src;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        try {
+            int[] arrayInteri = new int[5];
+            Random rand = new Random();
+            for (int i = 0; i < arrayInteri.length; i++) {
+                arrayInteri[i] = rand.nextInt(10) + 1;
+                System.out.println(arrayInteri[i]);
+            }
+
+            int userInput;
+            do {
+                System.out.print("Inserisci un numero (1-5) o 0 per uscire: ");
+                userInput = input.nextInt();
+
+                if (userInput >= 1 && userInput <= 5) {
+                    System.out.println("Hai selezionato l'elemento " + userInput + ": " + arrayInteri[userInput - 1]);
+                } else if (userInput != 0) {
+                    System.out.println("Input non valido. Inserisci un numero tra 1 e 5 o 0 per uscire.");
+                }
+            } while (userInput != 0);
+        } catch (NumberFormatException ex) {
+            System.out.println("Hai inserito un valore non numerico");
+        } catch (Exception ex) {
+            System.out.println("Errore generico");
+        } finally {
+            input.close();
+        }
+    }
+}
+
+
+
+
+
+
